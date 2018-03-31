83aa39fba8a65726e6af38c2ab39ae747ccad85c
Victor Pereira <victtor.vpb@gmail.com>
2018-03-30 21:58:25 -0300

    create model AutoMaker Closed #2

--- apps/crudvehicles/models.py
+++ apps/crudvehicles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class AutoMaker(models.Model):
+    name = models.CharField(max_length=100, blank=False,
+                            null=False, unique=True)
+
+    def __str__(self):
+        return self.name
