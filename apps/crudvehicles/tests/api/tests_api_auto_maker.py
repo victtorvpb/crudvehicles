2f03101d8c02979f018ef4ee24a6aebefbf337b3
Victor Pereira <victtor.vpb@gmail.com>
2018-03-31 19:11:37 -0300

    finish test api AutoMaker. Close #5

--- apps/crudvehicles/tests/api/tests_api_auto_maker.py
+++ apps/crudvehicles/tests/api/tests_api_auto_maker.py
@@ -4,7 +4,8 @@ from apps.crudvehicles.serializers import AutoMakerSerializer
 from django.urls import reverse
 from rest_framework import status
 from django.utils.http import urlencode
-
+import json
+from annoying.functions import get_object_or_None
 
 class TestAutoMakerListApi(TestCase):
     def setUp(self):
@@ -55,3 +56,88 @@ class TestAutoMakerListApi(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
+
+
+class TestAutoMakerCreateApi(TestCase):
+
+    def setUp(self):
+
+        AutoMaker.objects.create(
+            name='Ford'
+        )
+
+    def test_create_auto_maker(self):
+        data = {
+            "name": "Ferrari"
+        }
+
+        url = reverse('vehicles:AutoMaker-list')
+
+        response = self.client.post(url, data=data)
+
+        auto_maker = AutoMaker.objects.get(name=data.get('name'))
+        serializer = AutoMakerSerializer(auto_maker)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_create_not_unique_auto_maker(self):
+        data = {
+            "name": "Ford"
+        }
+
+        url = reverse('vehicles:AutoMaker-list')
+
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class TestAutoMakerUpdateApi(TestCase):
+
+    def setUp(self):
+
+        self.automaker_update = AutoMaker.objects.create(
+            name='Ford'
+        )
+
+    def test_update_auto_maker(self):
+        data = {
+            "name": "Honda"
+        }
+        new_data = json.dumps(data)
+        url = reverse('vehicles:AutoMaker-detail',
+                      kwargs={'pk': self.automaker_update.pk})
+
+        response = self.client.put(
+            url, data=new_data, content_type='application/json')
+
+        auto_maker = AutoMaker.objects.get(name=data.get('name'))
+        serializer = AutoMakerSerializer(auto_maker)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+
+class TestAutoMakerDeleteApi(TestCase):
+
+    def setUp(self):
+
+        self.automaker_delete = AutoMaker.objects.create(
+            name='Ford'
+        )
+
+    def test_update_auto_maker(self):
+ 
+        url = reverse('vehicles:AutoMaker-detail',
+                      kwargs={'pk': self.automaker_delete.pk})
+
+        response = self.client.delete(url)
+
+        auto_maker = get_object_or_None(AutoMaker, id=self.automaker_delete.pk)
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(None, auto_maker)
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
