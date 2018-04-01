76559d78ad3b8483cae55974548afeaf960d4a4f
Victor Pereira <victtor.vpb@gmail.com>
2018-03-31 23:48:48 -0300

    create test update VehicleModel

--- apps/crudvehicles/tests/api/tests_api_vehicle_model.py
+++ apps/crudvehicles/tests/api/tests_api_vehicle_model.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from django.utils.http import urlencode
 import json
 
+
 class TestVehicleModelMotorcycleListApi(TestCase):
     def setUp(self):
 
@@ -54,8 +55,8 @@ class TestVehicleModelMotorcycleListApi(TestCase):
 
         url = '{}?{}'.format(url, query)
         response = self.client.get(url)
-        vehicle_mode = self.objects_filter
-        serializer = VehicleModelSerializer(vehicle_mode, many=True)
+        vehicle_model = self.objects_filter
+        serializer = VehicleModelSerializer(vehicle_model, many=True)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
@@ -65,8 +66,8 @@ class TestVehicleModelMotorcycleListApi(TestCase):
                       kwargs={'pk': self.object_get.pk})
 
         response = self.client.get(url)
-        vehicle_mode = self.object_get
-        serializer = VehicleModelSerializer(vehicle_mode)
+        vehicle_model = self.object_get
+        serializer = VehicleModelSerializer(vehicle_model)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
@@ -119,8 +120,8 @@ class TestVehicleModelCarListApi(TestCase):
 
         url = '{}?{}'.format(url, query)
         response = self.client.get(url)
-        vehicle_mode = self.objects_filter
-        serializer = VehicleModelSerializer(vehicle_mode, many=True)
+        vehicle_model = self.objects_filter
+        serializer = VehicleModelSerializer(vehicle_model, many=True)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
@@ -130,8 +131,8 @@ class TestVehicleModelCarListApi(TestCase):
                       kwargs={'pk': self.object_get.pk})
 
         response = self.client.get(url)
-        vehicle_mode = self.object_get
-        serializer = VehicleModelSerializer(vehicle_mode)
+        vehicle_model = self.object_get
+        serializer = VehicleModelSerializer(vehicle_model)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
@@ -156,30 +157,98 @@ class TestVehicleModelCreateApi(TestCase):
         url = reverse('vehicles:VehicleModel-list')
 
         response = self.client.post(url, data=data)
-        
-        vehicle_mode = VehicleModel.objects.get(name=data.get('name'))
-        serializer = VehicleModelSerializer(vehicle_mode)
+
+        vehicle_model = VehicleModel.objects.get(name=data.get('name'))
+        serializer = VehicleModelSerializer(vehicle_model)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, serializer.data)
 
-
     def test_create_vehicle_mode_motorcycle(self):
 
         data = {
-            'name': 'Civic',
+            'name': 'CG',
             'model': ModelsTypesChoices.motorcycle.id,
-            'engine': 1.0,
+            'engine': 150,
             'automaker': self.auto_maker.pk
         }
 
         url = reverse('vehicles:VehicleModel-list')
 
         response = self.client.post(url, data=data)
-        
-        vehicle_mode = VehicleModel.objects.get(name=data.get('name'))
-        serializer = VehicleModelSerializer(vehicle_mode)
+
+        vehicle_model = VehicleModel.objects.get(name=data.get('name'))
+        serializer = VehicleModelSerializer(vehicle_model)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, serializer.data)
 
+
+class TestVehicleModelUpdateApi(TestCase):
+
+    def setUp(self):
+        auto_maker = AutoMaker.objects.create(
+            name='Honda'
+        )
+
+        self.auto_maker_update = AutoMaker.objects.create(
+            name='yamaha'
+        )
+
+        self.model_car_update = VehicleModel.objects.create(
+            name='Fit',
+            model=ModelsTypesChoices.car,
+            engine=1.0,
+            automaker=auto_maker
+        )
+
+        self.model_motorcycle_update = VehicleModel.objects.create(
+            name='Biz',
+            model=ModelsTypesChoices.motorcycle,
+            engine=150,
+            automaker=auto_maker
+        )
+
+    def test_update_partial_vehicle_model(self):
+        url = reverse('vehicles:VehicleModel-detail',
+                      kwargs={'pk': self.model_car_update.pk})
+
+        data = {
+            'name': 'Civic',
+            'engine': 2.0,
+        }
+
+        new_data = json.dumps(data)
+
+        response = self.client.patch(
+            url, data=new_data, content_type='application/json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        vehicle_model = VehicleModel.objects.get(name=data.get('name'))
+        serializer = VehicleModelSerializer(vehicle_model)
+
+        self.assertEqual(response.data, serializer.data)
+
+    def test_update_vehicle_model(self):
+        url = reverse('vehicles:VehicleModel-detail',
+                      kwargs={'pk': self.model_car_update.pk})
+
+        data = {
+            'name': 'Fazer',
+            'engine': 250,
+            'model': ModelsTypesChoices.motorcycle.id,
+            'automaker': self.auto_maker_update.id
+        }
+
+        new_data = json.dumps(data)
+
+        response = self.client.patch(
+            url, data=new_data, content_type='application/json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        vehicle_model = VehicleModel.objects.get(name=data.get('name'))
+        serializer = VehicleModelSerializer(vehicle_model)
+
+        self.assertEqual(response.data, serializer.data)
