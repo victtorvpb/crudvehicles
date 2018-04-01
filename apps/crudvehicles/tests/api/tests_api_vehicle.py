6edd847a37533a03877acee9c177171b904a5182
Victor Pereira <victtor.vpb@gmail.com>
2018-04-01 00:49:47 -0300

    fix test list Api Vehicles #7

--- apps/crudvehicles/tests/api/tests_api_vehicle.py
+++ apps/crudvehicles/tests/api/tests_api_vehicle.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from apps.crudvehicles.models import VehicleModel, AutoMaker, Vehicle
 from apps.crudvehicles.choices import ModelsTypesChoices
-from apps.crudvehicles.serializers import VehicleModelSerializer
+from apps.crudvehicles.serializers import VehicleSerializer
 from django.urls import reverse
 from rest_framework import status
 from django.utils.http import urlencode
@@ -9,7 +9,7 @@ import json
 from annoying.functions import get_object_or_None
 
 
-class TestVehicleModelListApi(TestCase):
+class TestVehicleListApi(TestCase):
     def setUp(self):
 
         auto_maker = AutoMaker.objects.create(
@@ -18,60 +18,64 @@ class TestVehicleModelListApi(TestCase):
         vehicle_model = VehicleModel.objects.create(
             name='Clio',
             model=ModelsTypesChoices.motorcycle,
-            engine=i*1.2,
+            engine=1.2,
             automaker=auto_maker,
         )
 
         for i in range(1, 4):
+            
             Vehicle.objects.create(
+                model= vehicle_model,
                 color='Branco',
                 mileage=i*100
             )
 
-        Vehicle.objects.create(
+        self.object_get =Vehicle.objects.create(
+            model= vehicle_model,
             color='Preto',
             mileage=1300
         )
 
         self.objects_list = Vehicle.objects.all()
+        # import pdb; pdb.set_trace()
         self.objects_filter_name = Vehicle.objects.filter(
-            vehicle_model__name__contains='Clio')
+            model__name__contains='Clio')
 
         self.objects_filter_color = Vehicle.objects.filter(
             color__contains='Branco')
 
-        self.objects_filter_engine_lte = VehicleModel.objects.filter(
-            vehicle_model__engine__lte=1.2)
+        self.objects_filter_engine_lte = Vehicle.objects.filter(
+            model__engine__lte=1.2)
 
-        self.objects_filter_engine_gte = VehicleModel.objects.filter(
-            vehicle_model__engine__gte=1.2)
+        self.objects_filter_engine_gte = Vehicle.objects.filter(
+            model__engine__gte=1.2)
 
     def test_list_vehicle(self):
-        url = reverse('vehicles:VehicleModel-list')
+        url = reverse('vehicles:Vehicle-list')
         response = self.client.get(url)
         vehicle_model = self.objects_list
-        serializer = VehicleModelSerializer(vehicle_model, many=True)
+        serializer = VehicleSerializer(vehicle_model, many=True)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
 
     def test_filter_vehicle_name(self):
-        url = reverse('vehicles:VehicleModel-list')
+        url = reverse('vehicles:Vehicle-list')
 
         query = urlencode({
-            'vehicle_model__name__contains': 'Clio'
+            'model__name__contains': 'Clio'
         })
 
         url = '{}?{}'.format(url, query)
         response = self.client.get(url)
         vehicle_model = self.objects_filter_name
-        serializer = VehicleModelSerializer(vehicle_model, many=True)
-
+        serializer = VehicleSerializer(vehicle_model, many=True)
+        
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
 
     def test_filter_vehicle_color(self):
-        url = reverse('vehicles:VehicleModel-list')
+        url = reverse('vehicles:Vehicle-list')
 
         query = urlencode({
             'color__contains': 'Branco'
@@ -80,46 +84,46 @@ class TestVehicleModelListApi(TestCase):
         url = '{}?{}'.format(url, query)
         response = self.client.get(url)
         vehicle_model = self.objects_filter_color
-        serializer = VehicleModelSerializer(vehicle_model, many=True)
+        serializer = VehicleSerializer(vehicle_model, many=True)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
 
     def test_get_vehicle(self):
-        url = reverse('vehicles:VehicleModel-detail',
+        url = reverse('vehicles:Vehicle-detail',
                       kwargs={'pk': self.object_get.pk})
 
         response = self.client.get(url)
         vehicle_model = self.object_get
-        serializer = VehicleModelSerializer(vehicle_model)
+        serializer = VehicleSerializer(vehicle_model)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
 
     def test_filter_engine_less(self):
-        url = reverse('vehicles:VehicleModel-list')
+        url = reverse('vehicles:Vehicle-list')
         query = urlencode({
-            'vehicle_model__engine__lte': 1.2
+            'model__engine__lte': 1.2
         })
 
         url = '{}?{}'.format(url, query)
         response = self.client.get(url)
         vehicle_model = self.objects_filter_engine_lte
-        serializer = VehicleModelSerializer(vehicle_model, many=True)
+        serializer = VehicleSerializer(vehicle_model, many=True)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
 
     def test_filter_engine_greater(self):
-        url = reverse('vehicles:VehicleModel-list')
+        url = reverse('vehicles:Vehicle-list')
         query = urlencode({
-            'vehicle_model__engine__gte': 1.2
+            'model__engine__gte': 1.2
         })
 
         url = '{}?{}'.format(url, query)
         response = self.client.get(url)
         vehicle_model = self.objects_filter_engine_gte
-        serializer = VehicleModelSerializer(vehicle_model, many=True)
+        serializer = VehicleSerializer(vehicle_model, many=True)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
